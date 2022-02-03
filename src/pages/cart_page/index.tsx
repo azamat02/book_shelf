1ea32f153e8f50409112a02e9e0ad49d0c3478a3
azamat02 <azamattolegenov1@gmail.com>
2022-02-03 15:27:42 +0600

    Added button to cart page to clean all items from cart

--- src/pages/cart_page/index.tsx
+++ src/pages/cart_page/index.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import CartBooksList from "../../components/cart-books-list";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {Trash} from "react-feather";
+import {cleanCart} from "../../store/cartSlice";
 
 export default function CartPage() {
     // @ts-ignore
     const cartBooks = useSelector(state => state.cart.books)
+    const dispatch = useDispatch()
+
+    const removeAllCart = () => {
+        dispatch(cleanCart());
+    }
 
     return (
         <div className={"cart-page-container"}>
             <p className="title">
                 Cart
             </p>
+
+            {
+                cartBooks.length ? (
+                    <button className={"clean-all-cart"} onClick={removeAllCart}>
+                        <Trash/>
+                        Clean all cart
+                    </button>
+                ) : ''
+            }
 
             <CartBooksList books={cartBooks}/>
         </div>
